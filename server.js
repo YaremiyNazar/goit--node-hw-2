83a1d2522153332885a0759b7c651ddb0184ec7f
Your Name <yaremiy.nazar@gmail.com>
2023-04-24 17:07:55 +0300

    add env-example

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
-  .then(() => app.listen(PORT))
-  .then(() => console.log("Database connection successful"))
+  .then(() => {
+    app.listen(PORT);
+    console.log("Database connection successful");
+  })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
